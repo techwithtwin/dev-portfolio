97ba734340466d2b72f2eddd7eefb3db3cac024a
techwithtwin <realwebtechguru@gmail.com>
2024-10-22 21:20:11 +0300

    ft:setup navbar

---
+++ components/navbar/index.tsx
@@ -0,0 +1,12 @@
+import { Flex } from "@chakra-ui/react";
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <Flex as="nav" bg="gray.800" color="white" p={4} justify="center">
+      Navbar
+    </Flex>
+  );
+};
+
+export default Navbar;
